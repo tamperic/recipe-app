b583776d714abffcc50102cecd6149528eb5f8e2
tamperic <tamaraperic.work@gmail.com>
2025-10-01 21:16:23 +0200

    Protect a FBV & CBV
    Enable access only after successful login

--- A2_Recipe_App/src/recipes/views.py
+++ A2_Recipe_App/src/recipes/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render     # Imported by default
 from django.views.generic import ListView, DetailView   # To display lists and details
 from .models import Recipe   # To access Recipe model 
+from django.contrib.auth.mixins import LoginRequiredMixin   # To protect class-based view so that view isn't displayed before successful login / authentication.
+from django.contrib.auth.decorators import login_required   # To protect function-based views
+
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/recipes_home.html', {'is_home': True})
 
-# Class-based view
-class RecipeListView(ListView):
+# Class-based view for recipe list
+class RecipeListView(ListView, LoginRequiredMixin):
     # Specify model
     model = Recipe
 
@@ -20,7 +23,14 @@ class RecipeListView(ListView):
         context['is_home'] = False
         return context
 
+# Class-based view for recipe details
+class RecipeDetailView(DetailView, LoginRequiredMixin):
     model = Recipe
-    template_name = 'recipes/detail.html'# Function-based view - records(request)
+    template_name = 'recipes/detail.html'
+
+
+# Function-based view - records(request)
+# Keep protected
+@login_required
 def records(request):
     return render(request, 'recipes/records.html')  # Display page
