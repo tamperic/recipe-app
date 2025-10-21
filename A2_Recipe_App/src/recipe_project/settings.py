2c3c1bb24fa8829213c41ee59cff1f417a0cf1b3
tamperic <tamaraperic.work@gmail.com>
2025-10-21 21:53:51 +0200

    Remove secret key from settings.py

--- A2_Recipe_App/src/recipe_project/settings.py
+++ A2_Recipe_App/src/recipe_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cc#t_p0imp*f32^!0k6m32tipqc+y!3*2_czb7-m*v6eyd1d8a'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
