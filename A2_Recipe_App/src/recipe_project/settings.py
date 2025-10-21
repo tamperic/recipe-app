2330c76760df2ec77551772142feb4db0966c8d9
tamperic <tamaraperic.work@gmail.com>
2025-10-21 21:30:13 +0200

    Change DEBUG to False

--- A2_Recipe_App/src/recipe_project/settings.py
+++ A2_Recipe_App/src/recipe_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cc#t_p0imp*f32^!0k6m32tipqc+y!3*2_czb7-m*v6eyd1d8a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
