184bda3f87c83ad50a506bf1bc15095600a439d3
tamperic <tamaraperic.work@gmail.com>
2025-10-29 19:57:43 +0100

    Fix ALLOWED_HOSTS

--- A2_Recipe_App/src/recipe_project/settings.py
+++ A2_Recipe_App/src/recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['recipe-app.onrender.com']
+ALLOWED_HOSTS = ['recipe-app-omgb.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
