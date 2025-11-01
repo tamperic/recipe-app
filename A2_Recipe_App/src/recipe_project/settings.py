3c8f54c3788603a9e7f5e76e755d48612eff764e
tamperic <tamaraperic.work@gmail.com>
2025-11-01 01:07:13 +0100

    Update DATABASES variable in settings

--- A2_Recipe_App/src/recipe_project/settings.py
+++ A2_Recipe_App/src/recipe_project/settings.py
@@ -80,14 +80,10 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 
 import dj_database_url
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',  # fallback to SQLite
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
